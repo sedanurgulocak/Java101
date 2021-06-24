6a322edc89c0e911fa8ef85a3bacb695989022d4
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-25 02:39:37 +0300

    ✨Suggest activities based on weather

---
+++ PatikaJava101/src/WeatherForecast/WeatherForecast.java
@@ -0,0 +1,31 @@
+package WeatherForecast;
+
+import java.util.Scanner;
+
+public class WeatherForecast {
+
+	public static void main(String[] args) {
+		int heat;
+		Scanner input = new Scanner (System.in);
+		System.out.println("Hava sýcaklýðýný giriniz: ");
+		heat = input.nextInt();
+		
+		if(heat < 5){
+			System.out.println("Kayak yapabilirsiniz");
+		}else if(heat >= 25) {
+			System.out.println("Yüzmeye gidebilirsiniz");
+		}else {
+			if(heat < 10) {
+				System.out.println("Sinemaya gidebilirsiniz");
+			}else if(heat > 20) {
+				System.out.println("Pikniðe gidebilirsiniz");
+			}else {
+				System.out.println("Sinemaya ve pikniðe gidebilirsiniz");
+			}
+			
+		}
+		input.close();
+
+	}
+
+}
