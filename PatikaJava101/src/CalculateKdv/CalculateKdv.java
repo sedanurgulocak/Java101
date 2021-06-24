9a2bc7231532c9784b25de552466e37cd58dbf33
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-24 16:43:52 +0300

    ✨Calculate KDV amount

---
+++ PatikaJava101/src/CalculateKdv/CalculateKdv.java
@@ -0,0 +1,27 @@
+package CalculateKdv;
+
+import java.util.Scanner;
+
+public class CalculateKdv {
+
+	public static void main(String[] args) {
+		double amount, kdv, kdvAmount;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Amount :");
+        amount = input.nextDouble();
+        if(amount >0 && amount <1000){
+            kdv = 0.18;
+            kdvAmount = amount * kdv;
+            System.out.println(kdvAmount);
+        }else if(amount>=1000){
+            kdv= 0.08;
+            kdvAmount = amount * kdv;
+            System.out.println(kdvAmount);
+        }else{
+            System.out.println("Sýfýrdan büyük deðer giriniz");
+        }
+        input.close();
+
+	}
+
+}
