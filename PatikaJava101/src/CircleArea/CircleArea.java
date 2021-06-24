492630229e77bc68b2c7c4598e1117198e9f7187
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-24 17:24:28 +0300

    ✨Calculate the area of ​​the circle segment

---
+++ PatikaJava101/src/CircleArea/CircleArea.java
@@ -0,0 +1,27 @@
+package CircleArea;
+
+import java.util.Scanner;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		int radius, angle;
+		double pi = 3.14;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Dairenin yarýçapýný giriniz : ");
+		radius = input.nextInt();
+		System.out.print("Merkez açýný ölçüsünü giriniz : ");
+		angle = input.nextInt();
+		
+		double temp = pi * (radius * radius) * angle;
+		double area = temp / 360;
+		
+		System.out.println("Daire diliminin alaný : " + area);
+		input.close();
+		
+		
+
+	}
+
+}
