2e3343f79fda41b0305d2a9390828c9d4f24175e
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-26 00:21:23 +0300

    ✨Sum of digits

---
+++ PatikaJava101/src/SumOfDigits/SumOfDigits.java
@@ -0,0 +1,23 @@
+package SumOfDigits;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		int number, counter = 0, temp = 0;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Sayý giriniz: ");
+		number = input.nextInt();
+		
+		while(number != 0) {
+			temp = (number % 10) + temp;
+			number /= 10;			
+		}
+		
+		System.out.println("Sonuc= " + temp);
+
+	}
+
+}
