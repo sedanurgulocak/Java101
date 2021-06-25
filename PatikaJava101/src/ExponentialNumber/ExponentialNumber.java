795761e660369a698800527b411a8df586001a39
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-25 23:51:35 +0300

    ✨Exponential number calculate

---
+++ PatikaJava101/src/ExponentialNumber/ExponentialNumber.java
@@ -0,0 +1,24 @@
+package ExponentialNumber;
+
+import java.util.Scanner;
+
+public class ExponentialNumber {
+
+	public static void main(String[] args) {
+		int x, y;
+		Scanner input =new Scanner(System.in);
+		System.out.print("Taban = ");
+		x = input.nextInt();
+		System.out.print("Üs = ");
+		y = input.nextInt();
+		int result = 1;
+		
+		for(int i=1; i<=y; i++) {
+			result *= x;
+		}
+		System.out.println("Sonuc = " + result);
+		input.close();
+
+	}
+
+}
