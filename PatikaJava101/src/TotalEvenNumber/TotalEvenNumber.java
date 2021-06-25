d71cab03dc44118c2c0bc274a0cee6a00307ec0e
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-25 19:32:29 +0300

    ✨Sum of even numbers up to odd numbers

---
+++ PatikaJava101/src/TotalEvenNumber/TotalEvenNumber.java
@@ -0,0 +1,26 @@
+package TotalEvenNumber;
+
+import java.util.Scanner;
+
+public class TotalEvenNumber {
+
+	public static void main(String[] args) {
+		int number, total =0;
+		Scanner input = new Scanner (System.in);
+
+		
+		do {
+			System.out.println("Sayý giriniz: ");
+			number = input.nextInt();
+			if(number % 2 == 0 && number % 4 == 0) {
+				total += number;
+			}
+			
+		}while(number %2 == 0);
+			
+		System.out.println("Toplam: " + total);
+		input.close();
+
+	}
+
+}
