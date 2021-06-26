27a945135bcc4e81f052750bb5c846d18b9a4019
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-27 01:42:28 +0300

    ✨Biggest smallest number

---
+++ PatikaJava101/src/BiggestSmallestNumber/BiggestSmallestNumber.java
@@ -0,0 +1,24 @@
+package BiggestSmallestNumber;
+
+import java.util.Scanner;
+
+public class BiggestSmallestNumber {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.println("Kaç tane sayý gireceksiniz?");
+		int x = input.nextInt();
+		int number;
+		
+		for(int i=1; i<= x; i++) {
+			System.out.print(i + ". Sayýyý giriniz: ");
+			number = input.nextInt();
+			System.out.println(number);
+		}
+		
+		
+		input.close();
+
+	}
+
+}
