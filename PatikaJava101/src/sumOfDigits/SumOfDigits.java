9ec582ce05be949ca8fd1a71e8180afbb68d2d40
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-27 11:04:35 +0300

    ♻️Rename package name to sumOfDigits

---
+++ PatikaJava101/src/sumOfDigits/SumOfDigits.java
@@ -0,0 +1,24 @@
+package sumOfDigits;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		int number, temp = 0;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Sayý giriniz: ");
+		number = input.nextInt();
+		
+		while(number != 0) {
+			temp = (number % 10) + temp;
+			number /= 10;			
+		}
+		
+		System.out.println("Sonuc= " + temp);
+		input.close();
+
+	}
+
+}
