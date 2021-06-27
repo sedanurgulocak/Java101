14a9d99716e9e0e3967908240be19d633686fa06
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-27 10:59:57 +0300

    ♻️Rename package name to combination

---
+++ PatikaJava101/src/combination/Combination.java
@@ -0,0 +1,31 @@
+package combination;
+
+import java.util.Scanner;
+
+public class Combination {
+
+	public static void main(String[] args) {
+		int n, nFak = 1, r, rFak = 1, temp = 1;
+		Scanner input = new Scanner(System.in);
+		System.out.println("n = " );
+		n = input.nextInt();
+		System.out.println("r = ");
+		r = input.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			nFak = nFak * i;
+		}
+		for(int i=1; i<=r; i++) {
+			rFak = rFak * i;
+		}
+		for(int i=1; i<=(n-r); i++) {
+			temp = temp * i;
+		}
+		
+		int result = nFak / (rFak * temp);
+		System.out.println("Kombinasyon= " + result);
+		input.close();	
+
+	}
+
+}
