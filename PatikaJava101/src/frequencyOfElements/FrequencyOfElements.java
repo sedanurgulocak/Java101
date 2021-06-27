80d25b0cefde33e86d6c5c7b0d241df267ab2a0b
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-27 23:22:50 +0300

    ✨Find frequency of elements

---
+++ PatikaJava101/src/frequencyOfElements/FrequencyOfElements.java
@@ -0,0 +1,27 @@
+package frequencyOfElements;
+
+import java.util.Scanner;
+
+public class FrequencyOfElements {
+
+	public static void main(String[] args) {
+		int counter=0;
+        
+        int[]array={10,5,7,8,45,9,10,45,78,32};
+                
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Dizide Aratmak Ýstediðiniz Sayýyý Giriniz");
+        int search=klavye.nextInt();
+        
+        for(int i=0;i<array.length;i++){
+            if(search==array[i]){
+                
+                counter=counter+1;
+            }  
+        
+    }
+        System.out.println("Dizide Aradýðýnýz Sayý    : "   + search);
+        System.out.println("Dizide Tekrar Edilme Sayýsý : " + counter);
+    }
+
+}
