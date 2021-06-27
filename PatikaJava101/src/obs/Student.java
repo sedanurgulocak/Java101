fe7a4cca0339ee77ca1fe681feea7ca195b5ce28
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-27 22:09:24 +0300

    ✨Student information system

---
+++ PatikaJava101/src/obs/Student.java
@@ -0,0 +1,71 @@
+package obs;
+
+public class Student {
+	String name, stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+    double fizikAvarage, matAvarage, kimyaAvarage;
+
+    Student(String name, int classes, String stuNo, Course mat, Course fizik, Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiþ");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sýnýfý Geçti. ");
+            } else {
+                System.out.println("Sýnýfta Kaldý.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        this.fizikAvarage = (this.fizik.sozluHesapla(this.fizik.sozlu) * 0.20) + (this.fizik.note * 0.80);
+        this.matAvarage = (this.mat.sozluHesapla(this.kimya.sozlu) * 0.20) + (this.mat.note * 0.80);
+        this.kimyaAvarage = (this.kimya.sozluHesapla(this.kimya.sozlu) * 0.20) + (this.kimya.note * 0.80);
+        this.avarage = (this.fizik.note + this.kimya.note + this.mat.note) / 3;
+    }
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote() {
+        System.out.println("=========================");
+        System.out.println("Öðrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+    }
+}
