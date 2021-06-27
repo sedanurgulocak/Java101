0640d827b46261a591e5b7042c9027b48af3844e
sedanurgulocak <sedanurgulocak@gmail.com>
2021-06-27 11:00:28 +0300

    ♻️Rename package name to divisibility

---
+++ PatikaJava101/src/divisibility/Divisibility.java
@@ -0,0 +1,25 @@
+package divisibility;
+
+import java.util.Scanner;
+
+public class Divisibility {
+
+	public static void main(String[] args) {
+		int number, total=0, average=0, counter =0;
+		Scanner input=new Scanner(System.in);
+		System.out.println("Sayý giriniz: ");
+		number = input.nextInt();
+		
+		for(int i=0; i<=number; i++) {
+			if(i % 3 == 0 && i % 4 == 0) {
+				total += i;
+				counter++;
+			}
+		}
+		average = total/counter;
+		System.out.println("Ortalama: "+ average);
+		input.close();
+
+	}
+
+}
